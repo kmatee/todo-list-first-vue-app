aeb4b523d03422552cff08aa4cae6eb7d4cfa0fa
kantm <kantm@DESKTOP-H26FR5O>
2023-06-07 19:31:09 +0200

    Add radio button to select between personal and business category

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,10 @@ onMounted(() => {
   name.value = localStorage.getItem('name') || ''
 })
 
+const addTodo = () => {
+
+}
+
 </script>
 
 <template>
@@ -27,6 +31,43 @@ onMounted(() => {
       <h2 class="title">
         What's up, <input type="text" placeholder="Name here" v-model="name">
       </h2>
+    </section>
+
+    <section class="create-todo">
+      <h3>CREATE A TODO</h3>
+
+      <form @submit.prevent="addTodo">
+        <h4>What's on your todo list?</h4>
+        <input
+          type="text"
+          placeholder="e.g. Start coding"
+          v-model="input_content">
+        <h4>Pick a category</h4>
+
+        <div class="options">
+          <label>
+            <input
+              type="radio"
+              name="category"
+              value="business"
+              v-model="input_category">
+              <span class="bubble business"></span>
+              <div>Business</div>
+          </label>
+        </div>
+
+        <div class="options">
+          <label>
+            <input
+              type="radio"
+              name="category"
+              value="personal"
+              v-model="input_category">
+              <span class="bubble personal"></span>
+              <div>Personal</div>
+          </label>
+        </div>
+      </form>
     </section>
 </main>
 </template>
